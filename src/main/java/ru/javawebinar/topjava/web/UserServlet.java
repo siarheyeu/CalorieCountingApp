d150468fbb59f7dd9b8c64e80ad91ca92d9099db
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-02-16 00:02:36 +0300

    Forward to redirect

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -11,6 +11,7 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
